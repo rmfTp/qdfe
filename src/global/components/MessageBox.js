d6e1e15e483e1623bf4e89597755e560e235139e
rmftp <kyj2001kyj@naver.com>
2025-07-10 17:23:01 +0900

    o

--- src/global/components/MessageBox.js
+++ src/global/components/MessageBox.js
@@ -1,7 +1,42 @@
 import React from 'react';
-const MessageBox = ({ items }) => {
-  console.log('items', items);
-  return <></>;
+import styled, { css } from 'styled-components';
+import color from '../styles/color';
+import fontsize from '../styles/fontsize';
+
+const { primary } = color;
+const { normal } = fontsize;
+const StyledItem = styled.div`
+  box-shadow: 2px 2px 5px ${primary};
+  color: ${primary};
+  padding: 5px 10px;
+  text-align: center;
+  border-radius: 3px;
+  background: #fff;
+  & + & {
+    margin-top: 5px;
+  }
+  ${(theme) =>
+    theme &&
+    color[theme] &&
+    css`
+      box-shadow: 2px 2px 5px ${color[theme]};
+      color: ${color[theme]};
+    `}
+`;
+const MessageBox = ({ items, theme, children }) => {
+  if (children) items = children;
+  items = Array.isArray(items) ? items : items ? [items] : [];
+  if (items.length === 0) return <></>;
+
+  return (
+    <>
+      {items.map((item, i) => (
+        <StyledItem key={item + '-' + i} theme={theme ?? 'primary'}>
+          {item}
+        </StyledItem>
+      ))}
+    </>
+  );
 };
 
 export default React.memo(MessageBox);
--- src/member/components/LoginForm.js
+++ src/member/components/LoginForm.js
@@ -3,51 +3,57 @@ import styled from 'styled-components';
 import { MediumButton } from '../../global/components/Buttons';
 import color from '../../global/styles/color';
 import fontsize from '../../global/styles/fontsize';
-import LoginContainer from '../containers/LoginContainer';
 import MessageBox from '../../global/components/MessageBox';
 
 const { dark } = color;
 const { medium } = fontsize;
+
 const StyledForm = styled.form`
   min-width: 280px;
   max-width: 450px;
   width: 100%;
   margin: 200px auto 0;
+
   input {
     border: 1px solid ${dark};
     width: 100%;
     height: 55px;
     display: block;
     font-size: ${medium};
-    border-radius: 3px;
     text-align: center;
-    + input {
-      margin-top: 10px;
-    }
+    border-radius: 3px;
   }
+
+  input + input {
+    margin-top: 10px;
+  }
+
   button {
-    margin-top: 30px;
+    margin-top: 10px;
   }
 `;
+
 const LoginForm = ({ form, onChange, onSubmit, errors }) => {
+  console.log('errors', errors);
   return (
-    <StyledForm onSubmit={onSubmit} autoComplete="off" errors={errors}>
+    <StyledForm onSubmit={onSubmit} autoComplete="off">
       <input
         type="text"
-        placeholder="이메일"
         name="email"
+        placeholder="이메일"
         value={form.email ?? ''}
         onChange={onChange}
       />
-      <MessageBox items={errors.email}></MessageBox>
+      <MessageBox theme="danger">{errors.email}</MessageBox>
       <input
         type="password"
-        placeholder="비밀번호"
         name="password"
+        placeholder="비밀번호"
         value={form.password ?? ''}
         onChange={onChange}
       />
-      <MessageBox items={errors.email}></MessageBox>
+      <MessageBox theme="danger">{errors.password}</MessageBox>
+
       <MediumButton type="submit" width="100%">
         로그인
       </MediumButton>
